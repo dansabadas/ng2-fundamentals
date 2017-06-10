013417d7c6f15871e98c460bad5b0f5d88648756
DESKTOP-FSNB2S3\dan_s <dan.sabadas@gmail.com>
2017-06-10 14:09:05 +0300

    communicating with child component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
     imports: [BrowserModule],
     exports: [],
-    declarations: [EventsAppComponent],
+    declarations: [EventsListComponent, EventsAppComponent, EventThumbnailComponent],
     providers: [],
     bootstrap:[EventsAppComponent]
 })
--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'events-app',
-    template: '<h2>hello world</h2>'
+    template: '<events-list></events-list>'
 })
 
 export class EventsAppComponent implements OnInit {
