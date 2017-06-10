6a9060720b2119a58166064124eb53e3460bbe3c
DESKTOP-FSNB2S3\dan_s <dan.sabadas@gmail.com>
2017-06-10 11:30:27 +0300

    finished creating first component

---
+++ app/events-app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'events-app',
+    template: '<h2>hello world</h2>'
+})
+
+export class EventsAppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
+
