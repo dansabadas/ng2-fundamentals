7028b6ce73c9ff08f1dc478e7e50e040beeb4523
AD001\z003p8vs <dan.sabadas@gmail.com>
2017-06-24 08:09:43 +0300

    arrived at 12: directives with adding jquery

--- app/events/shared/event.service.ts
+++ app/events/shared/event.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core'
+import { Injectable, EventEmitter } from '@angular/core'
 import { Subject, Observable } from 'rxjs/RX'
 
 import { IEvent } from './event.model'
+import { ISession } from './session.model'
 
 @Injectable()   // this Injectable decorator is not really required
 export class EventService {
@@ -35,6 +36,27 @@ export class EventService {
   updateEvent(event) {
     let index = EVENTS.findIndex(x => x.id = event.id)
     EVENTS[index] = event
+  }
+
+  searchSessions(searchTerm: string) : EventEmitter<any> {
+    var term = searchTerm.toLocaleLowerCase();
+    var results: ISession[] = [];
+
+    EVENTS.forEach(event => {
+      var matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
+      matchingSessions = matchingSessions.map((session:any) => {
+        session.eventId = event.id;
+        return session;
+      });
+      results = results.concat(matchingSessions);
+    })
+
+    var emitter = new EventEmitter(true); // 'true' argument means deliver this event async
+    setTimeout(() => {  // we put set-timeout to simulate http call
+      emitter.emit(results);
+    }, 100);
+
+    return emitter;
   }
 }
 
